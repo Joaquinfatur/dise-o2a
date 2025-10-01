a1890c01aec6c8a8d0b11917799c6edd0b810596
unknown <jfatur@frba.utn.edu.ar>
2025-10-01 01:42:31 -0300

    Prueba temporal

--- src/main/java/ar/edu/utn/dds/k3003/Application.java
+++ src/main/java/ar/edu/utn/dds/k3003/Application.java
@@ -1,12 +1,28 @@
 package ar.edu.utn.dds.k3003;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Application {
-    
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
+    
+    @Bean
+    public CommandLineRunner verifyDatabase(javax.sql.DataSource dataSource) {
+        return args -> {
+            try {
+                String url = dataSource.getConnection().getMetaData().getURL();
+                System.out.println("========================================");
+                System.out.println(" CONECTADO A: " + url);
+                System.out.println("========================================");
+            } catch (Exception e) {
+                System.err.println(" ERROR CONECTANDO DB: " + e.getMessage());
+            }
+        };
+    }
+    
 }
